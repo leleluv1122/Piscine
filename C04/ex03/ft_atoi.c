6ef97b39abaa0a64c82823851489be31c06d0740
leleluv1122 <jh_o214@naver.com>
2020-10-28 21:47:44 +0900

    c04 ~ing

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,30 +6,54 @@
 /*   By: seupark <seupark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 20:17:08 by seupark           #+#    #+#             */
-/*   Updated: 2020/10/28 20:29:41 by seupark          ###   ########.fr       */
+/*   Updated: 2020/10/28 21:47:18 by seupark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-int		ft_atoi(char *str)
+int	check_space(char c)
 {
-	int		minus;
-	int		idx;
+	if (c == ' ')
+		return (1);
+	else if (c == '\t')
+		return (1);
+	else if (c == '\n')
+		return (1);
+	else if (c == '\v')
+		return (1);
+	else if (c == '\f')
+		return (1);
+	else if (c == '\r')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(char *str)
+{
+	int	idx;
+	int	result;
+	int	minus;
 
 	idx = 0;
+	result = 0;
 	minus = 0;
 	while (str[idx] != '\0')
 	{
-		
-
+		if (check_space(str[idx]) || str[idx] == '+')
+			;
+		else if (str[idx] == '-')
+			minus++;
+		else if (str[idx] >= '0' && str[idx] <= '9')
+		{
+			result *= 10;
+			result += (str[idx] - '0');
+		}
+		else
+			break ;
 		idx++;
 	}
-}
-
-int main(){
-	char str[] = "---154aa";
-	printf("%d", ft_atoi(str));
+	if (minus % 2 == 1)
+		result *= -1;
+	return (result);
 }
