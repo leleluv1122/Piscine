1556dca17d11910e840d9d7763c3ccd82ac457e8
leleluv1122 <jh_o214@naver.com>
2020-11-04 17:48:20 +0900

    edit

--- C08/ex05/ft_show_tab.c
+++ C08/ex05/ft_show_tab.c
@@ -6,13 +6,46 @@
 /*   By: seupark <seupark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 17:38:14 by seupark           #+#    #+#             */
-/*   Updated: 2020/11/04 17:39:17 by seupark          ###   ########.fr       */
+/*   Updated: 2020/11/04 17:48:12 by seupark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	print_str(char *str)
+{
+	while(*str)
+	{
+		write(1, str++, 1);
+	}
+	write(1, "\n", 1);
+}
+
+void	print_int(int n)
+{
+	char	c;
+
+	if (n < 10)
+	{
+		c = n + '0';
+		write(1, &c, 1);
+	}
+	else
+	{
+		print_int(n / 10);
+		print_int(n % 10);
+	}
+}
+
 void	ft_show_tab(struct s_stock_str *par)
 {
+	int		i;
 
+	i = 0;
+	while (par[i])
+	{
+		print_str(par[i].str);
+		print_int(par[i].size);
+		print_str(par[i].copy);
+	}
 }
