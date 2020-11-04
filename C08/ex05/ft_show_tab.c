b599b1cb498e8f290c651a792c37df4b3c9a694e
leleluv1122 <jh_o214@naver.com>
2020-11-04 18:02:39 +0900

    edit

--- C08/ex05/ft_show_tab.c
+++ C08/ex05/ft_show_tab.c
@@ -6,7 +6,7 @@
 /*   By: seupark <seupark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 17:38:14 by seupark           #+#    #+#             */
-/*   Updated: 2020/11/04 17:48:12 by seupark          ###   ########.fr       */
+/*   Updated: 2020/11/04 17:52:49 by seupark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	print_str(char *str)
 {
-	while(*str)
+	while (*str)
 	{
 		write(1, str++, 1);
 	}
@@ -46,6 +46,7 @@ void	ft_show_tab(struct s_stock_str *par)
 	{
 		print_str(par[i].str);
 		print_int(par[i].size);
+		write(1, "\n", 1);
 		print_str(par[i].copy);
 	}
 }
